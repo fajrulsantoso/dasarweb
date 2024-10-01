d26b6d145b7b0d22aebd1328bde6dd388ab1e468
fajrul <fajrulsantoso10@gmail.com>
2024-10-01 08:43:29 +0700

    Praktikum_6

---
+++ Praktikum_6/fungsi.php
@@ -0,0 +1,11 @@
+<?php 
+function perkenalan(){
+    echo "Asalamualakium, ";
+    echo "Perkenalan, nama saya elok<br>";
+    echo "senang berkenalam dengan Anda <br>";
+}
+  //memanggil fungsi yang sudah dibuat
+   perkenalan();
+
+   
+?>
